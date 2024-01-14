24eb0b77a9ed7b8256ad1237833c75ab5c246d48
vahmtrade <vahmtrade@gmail.com>
2024-01-14 18:29:03 +0330

    Convert the check_stock_files function into separate functions

--- preprocess/basic.py
+++ preprocess/basic.py
@@ -24,15 +24,6 @@ def pairwise(iterable):
     return zip(a, b)
 
 
-def all_dict_values(data: dict):
-    """data : {"A": 1,"B": {"C": 2,"D": {"E": 3}}} => [1,2,3]"""
-    for v in data.values():
-        if isinstance(v, dict):
-            yield from all_dict_values(v)
-        else:
-            yield v
-
-
 def only_zero_inequality(n):
     """Accepts only non-zero values"""
     if isinstance(n, (int, float)) and n != 0:
@@ -138,8 +129,8 @@ def clarify_number(a, seprator=",", n=2):
 
 def move_last_file(new_path, base_path=DB):
     filename = max(
-        os.listdir(DB),
-        key=lambda x: os.path.getctime(os.path.join(DB, x)),
+        os.listdir(base_path),
+        key=lambda x: os.path.getctime(os.path.join(base_path, x)),
     )
     old_path = f"{base_path}/{filename}"
     if os.path.isfile(old_path):
@@ -150,7 +141,6 @@ def load_windows_app(app_name):
     try:
         app = win32.gencache.EnsureDispatch(app_name)
     except AttributeError:
-        # remove cache and try again
         MODULE_LIST = [m.__name__ for m in sys.modules.values()]
         for module in MODULE_LIST:
             if re.match(r"win32com\.gen_py\..+", module):
@@ -206,14 +196,6 @@ def get_excel_nums(file_path):
     return nums
 
 
-def essential_stock_files(stock_name):
-    return [
-        f"{INDUSPATH}/{wl_prod[stock_name]['indus']}/{stock_name}/{i}"
-        for i in all_dict_values(structure)
-        if ".xlsx" in i
-    ]
-
-
 def list_stock_files(stock_name):
     stock_dirs = []
     stock_files = []
@@ -227,6 +209,23 @@ def list_stock_files(stock_name):
             stock_files.append(os.path.join(path, name).replace("\\", "/"))
 
     return stock_dirs, stock_files
+
+
+def all_dict_values(data: dict):
+    """data : {"A": 1,"B": {"C": 2,"D": {"E": 3}}} => [1,2,3]"""
+    for v in data.values():
+        if isinstance(v, dict):
+            yield from all_dict_values(v)
+        else:
+            yield v
+
+
+def essential_stock_files(stock_name):
+    return [
+        f"{INDUSPATH}/{wl_prod[stock_name]['indus']}/{stock_name}/{i}"
+        for i in all_dict_values(structure)
+        if ".xlsx" in i
+    ]
 
 
 def create_database_structure():
@@ -243,150 +242,160 @@ def create_database_structure():
     Path(PKLPATH).mkdir(parents=True, exist_ok=True)
 
 
-def filepath_info(filepath):
-    article_type, per_article_type, time_type = "", "", ""
-    try:
-        for i in article_types_dict:
-            if i in filepath:
-                article_type = i
-
-        per_article_type = article_types_dict[article_type]
-
-    except Exception as err:
-        print("cant get type:", filepath, err)
-
-    try:
-        for i in ["monthly", "quarterly", "yearly"]:
-            if i in filepath:
-                time_type = i
-
-    except Exception as err:
-        print("cant get time:", filepath, err)
-
-    return [article_type, per_article_type, time_type]
-
-
 def find_deficiencies(stock_name):
-    """deficiencies, pe, opt, eps"""
-
     base_files = essential_stock_files(stock_name)
-    missing_files = [i for i in base_files if not Path(i).exists()]
+    missing_filepaths = [i for i in base_files if not Path(i).exists()]
+    article_types, time_types, pe, opt, eps = [], [], False, False, False
 
-    article_types, time_types = [], []
-    pe, opt, eps = False, False, False
-
-    for file in missing_files:
-        if "eps.xlsx" in file:
+    for filepath in missing_filepaths:
+        if "eps.xlsx" in filepath:
             eps = True
-        elif "opt.xlsx" in file:
+        elif "opt.xlsx" in filepath:
             opt = True
-        elif "pe.xlsx" in file:
+        elif "pe.xlsx" in filepath:
             pe = True
-
         else:
-            article_type, per_article_type, time_type = filepath_info(file)
+            article_type = next((i for i in article_types_dict if i in filepath), "")
+            time_type = next(
+                (i for i in ["monthly", "quarterly", "yearly"] if i in filepath), ""
+            )
             article_types.append(article_type)
             time_types.append(time_type)
 
     deficiencies = defaultdict(list)
-
     for key, value in zip(article_types, time_types):
         deficiencies[key].append(value)
-    deficiencies = dict(deficiencies)
 
-    for i in deficiencies:
-        deficiencies[i] = list(set(deficiencies[i]))
+    return {i: list(set(deficiencies[i])) for i in deficiencies}, pe, opt, eps
 
-    return deficiencies, pe, opt, eps
+
+def not_pe_opt_eps(filepath):
+    return not any(x in filepath for x in ["pe.xlsx", "eps.xlsx", "opt.xlsx"])
+
+
+def filepath_info(filepath):
+    if not_pe_opt_eps(filepath):
+        article_type = next((i for i in article_types_dict if i in filepath), "")
+        per_article_type = article_types_dict.get(article_type, "")
+        time_types = ["monthly", "quarterly", "yearly"]
+        time_type = next((i for i in time_types if i in filepath), "")
+        return [article_type, per_article_type, time_type]
+
+    else:
+        return [False, False, False]
+
+
+def is_stock_file_large_size(filepath):
+    """Returns True if the file size is larger than 1 MB, False otherwise"""
+
+    file_size = (os.stat(filepath).st_size) / (1024 * 1024)
+    file_ctime = JalaliDate(datetime.fromtimestamp(os.path.getctime(filepath)))
+    if file_size > 1:
+        print("large file :", filepath, file_ctime)
+        return True
+    else:
+        return False
+
+
+def is_stock_file_empty(df, filepath):
+    """Returns True if the file is empty, False otherwise"""
+
+    if df.applymap(only_zero_inequality).isnull().all().all():
+        print("empty file :", filepath)
+        return True
+    else:
+        return False
+
+
+def is_stock_file_not_match_time(
+    df, filepath, time_type, last_year, last_quarter, last_month
+):
+    """Returns True if the file time does not match the expected time, False otherwise"""
+
+    if not_pe_opt_eps(filepath):
+        excel_timeids = re.findall(regex_en_timeid_q, str(df.loc[6]))
+        excel_years = list(map(lambda x: int(x.split("/")[0]), excel_timeids))
+        excel_months = list(map(lambda x: int(x.split("/")[1]), excel_timeids))
+        excel_steps = [abs(a - b) for a, b in pairwise(excel_months)]
+        excel_step = max(excel_steps, key=excel_steps.count)
+        excel_last_quarter = ((excel_months[-1] - 1) // 3) + 1
+
+        steptypes = {1: "monthly", 3: "quarterly", 0: "yearly"}
+        if steptypes[excel_step] != time_type:
+            print("unmatch time :", filepath)
+            return True
+
+        if time_type == "monthly" and last_month != False:
+            if str(last_month) != str(excel_months[-1]):
+                print("old data monthly :", filepath, excel_months[-1])
+                return True
+
+        if time_type == "quarterly" and last_quarter != False:
+            if str(last_quarter) != str(excel_last_quarter):
+                print("old data quarterly :", filepath, excel_last_quarter)
+                return True
+
+        if time_type == "yearly" and last_year != False:
+            if str(last_year) != str(excel_years[-1]):
+                print("old data yearly :", filepath, excel_years[-1])
+                return True
+
+        return False
+
+
+def is_stock_file_not_match_author_type(df, filepath, per_article_type, stock_name):
+    """Returns True if the file author or type does not match the expected values, False otherwise"""
+
+    if not_pe_opt_eps(filepath):
+        author = "Pouya Finance"
+        excel_author = df["Unnamed: 1"][0]
+        excel_type = df["Unnamed: 1"][4]
+        excel_token = (df["Unnamed: 1"][3]).replace("\u200c", "").split("-")[0]
+
+        if excel_author != author:
+            print("not bourseview : ", filepath)
+            return True
+
+        if excel_type != per_article_type:
+            print("unmatch type : ", filepath, per_article_type)
+            return True
+
+        if excel_token != wl_prod[stock_name]["token"]:
+            print("unmatch name :", filepath)
+            return True
+
+        return False
 
 
 def check_stock_files(
     stock_name, last_year=False, last_quarter=False, last_month=False, delete=False
 ):
     failed = []
-    base_files = essential_stock_files(stock_name)
 
-    for file in base_files:
-        if Path(file).exists():
-            file_size = (os.stat(file).st_size) / (1024 * 1024)
-            file_ctime = JalaliDate(datetime.fromtimestamp(os.path.getctime(file)))
-
-            if file_size > 1:
-                print("large file :", file)
-                failed.append(file)
-
+    for filepath in essential_stock_files(stock_name):
+        if Path(filepath).exists():
             try:
-                df = pd.read_excel(file)
-                if df.applymap(only_zero_inequality).isnull().all().all():
-                    print("empty file :", file)
-                    failed.append(file)
+                df = pd.read_excel(filepath)
+                article_type, per_article_type, time_type = filepath_info(filepath)
+                if any(
+                    [
+                        is_stock_file_large_size(filepath),
+                        is_stock_file_empty(df, filepath),
+                        is_stock_file_not_match_time(
+                            df, filepath, time_type, last_year, last_quarter, last_month
+                        ),
+                        is_stock_file_not_match_author_type(
+                            df, filepath, per_article_type, stock_name
+                        ),
+                    ]
+                ):
+                    failed.append(filepath)
 
             except Exception as err:
-                print("old format : ", file, err)
-                failed.append(file)
+                print(err, filepath)
+                failed.append(filepath)
 
-            try:
-                article_type, per_article_type, time_type = filepath_info(file)
-
-            except Exception as err:
-                print("cant get filepath info :", file, err)
-
-            try:
-                excel_timeids = re.findall(regex_en_timeid_q, str(df.loc[6]))
-                excel_years = list(map(lambda x: int(x.split("/")[0]), excel_timeids))
-                excel_months = list(map(lambda x: int(x.split("/")[1]), excel_timeids))
-                excel_steps = [abs(a - b) for a, b in pairwise(excel_months)]
-                excel_step = max(excel_steps, key=excel_steps.count)
-                excel_last_quarter = ((excel_months[-1] - 1) // 3) + 1
-
-                steptypes = {1: "monthly", 3: "quarterly", 0: "yearly"}
-                if steptypes[excel_step] != time_type:
-                    print("unmatch time :", file)
-                    failed.append(file)
-
-                if time_type == "monthly" and last_month != False:
-                    if str(last_month) != str(excel_months[-1]):
-                        print("old data monthly :", file, excel_months[-1])
-                        failed.append(file)
-
-                if time_type == "quarterly" and last_quarter != False:
-                    if str(last_quarter) != str(excel_last_quarter):
-                        print("old data quarterly :", file, excel_months[-1])
-                        failed.append(file)
-
-                if time_type == "yearly" and last_year != False:
-                    if str(last_year) != str(excel_years[-1]):
-                        print("old data yearly :", file, excel_years[-1])
-                        failed.append(file)
-
-            except Exception as err:
-                # TODO : remove pe,opt,eps from this exception
-                # print("cant get excel timeids :", file, err)
-                print(err)
-
-            try:
-                excel_author = df["Unnamed: 1"][0]
-                excel_type = df["Unnamed: 1"][4]
-                excel_token = (df["Unnamed: 1"][3]).replace("\u200c", "").split("-")[0]
-
-                if excel_author != "Pouya Finance":
-                    print("not bourseview : ", file)
-                    failed.append(file)
-
-                if excel_type != per_article_type:
-                    print("unmatch type : ", file, per_article_type)
-                    failed.append(file)
-
-                if excel_token != wl_prod[stock_name]["token"]:
-                    print("unmatch name :", file)
-                    failed.append(file)
-
-            except Exception as err:
-                # TODO : remove pe,opt,eps from this exception
-                # print("cant get excel author :", file, err)
-                print(err)
-
-    failed = list(set(failed))
-    if delete:
-        for i in failed:
+    for i in set(failed):
+        print(i)
+        if delete:
             os.remove(i)
